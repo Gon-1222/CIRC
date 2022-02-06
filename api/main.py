98553b68715856b897914e8ab52841ac8e57da8d
Gon-1222 <caramelpopsoft@gmail.com>
2022-02-06 16:27:51 +0900

    debug 3

--- api/main.py
+++ api/main.py
@@ -134,6 +134,6 @@ def handle_follow(event):
 @handler.add(UnfollowEvent)
 def handle_unfollow(event):
     Friends.remove(event.source.user_id)
-
+print("2")
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=False)
