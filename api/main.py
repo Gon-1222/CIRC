52e3554b048f8b7486311c0f69c85a729400a9f4
Gon-1222 <caramelpopsoft@gmail.com>
2022-02-06 17:05:10 +0900

    debug 11

--- api/main.py
+++ api/main.py
@@ -10,3 +10,12 @@ import json
 import datetime
 CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+versions='beta1'
+Flax=Flax()
+#line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+h#andler = WebhookHandler(LINE_CHANNEL_SECRET)
+Friends = friend()
+Schedule  =Schedular()
+
+app = Flask(__name__)
