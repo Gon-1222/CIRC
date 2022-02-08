de37c87ca10fd9372050c86ae18d90e50705ec54
Gon-1222 <caramelpopsoft@gmail.com>
2022-02-08 21:32:30 +0900

    beta1

--- api/main.py
+++ api/main.py
@@ -11,7 +11,7 @@ import datetime
 CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-versions='beta1'
+versions='Working on Vercel.com \n CIRC-SheduleManager\n(beta1)'
 Flax=Flax()
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handle = WebhookHandler(LINE_CHANNEL_SECRET)
