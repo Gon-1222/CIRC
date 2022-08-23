b61fb0d83ef2491605a16da227dc5388b89983cf
Gon-1222 <caramelpopsoft@gmail.com>
2022-08-23 15:27:14 +0900

    aiueo

--- api/static/js/lazy_part.js
+++ api/static/js/lazy_part.js
@@ -1,13 +1,13 @@
 $.get('/party')
   .done(function(data) {
-    $('main').html(data);
+    $('main').html(data).trigger('create');
   }).fail(function() {
     $('.loading').html('<i class="fa-solid fa-face-frown"></i><div class="japanese">取得できませんでした</div><div class="japanese">再読込してください</div>');
   })
 
 $.get('/news')
   .done(function(data) {
-    $('.news-banner__content').html(data);
+    $('.news-banner__content').html(data).trigger('create');
 
   }).fail(function() {
     $('.news-banner__content').html('<p><i class="fa-solid fa-face-frown"></i>取得できませんでした。</p>');
