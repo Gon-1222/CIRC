ab4cf2602769f834ded87e03072770b3245c48d4
Gon-1222 <caramelpopsoft@gmail.com>
2022-08-23 13:13:10 +0900

    aaaaa

--- api/static/js/lazy_part.js
+++ api/static/js/lazy_part.js
@@ -2,10 +2,10 @@ $.get('/party')
 .done(function( data ) {
 
     $('main').html( data );
-    $('main').html( '<div class="reload_mark"><p><i class="fa-solid fa-face-confused"></i>取得できませんでした。</p><p>再読込してください。</p></div>' );
+    $('.loading').html( '<div class="reload_mark"><p><i class="fa-solid fa-face-confused"></i>取得できませんでした。</p><p>再読込してください。</p></div>' );
 
 }).fail( function() {
-  $('main').html( '<div class="reload_mark"><p><i class="fa-solid fa-face-confused"></i>取得できませんでした。</p><p>再読込してください。</p></div>' );
+  $('.loading').html( '<i class="fa-solid fa-face-confused"></i><div class="japanese">取得できませんでした。</div><div>再読込してください。</div>' );
 } )
 
 $.get('/news')
