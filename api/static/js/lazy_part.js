d91f9ae03bb21884ba1b02e0aa4e8e51749f53b4
Gon-1222 <caramelpopsoft@gmail.com>
2022-08-23 13:28:01 +0900

    aaaaaa

--- api/static/js/lazy_part.js
+++ api/static/js/lazy_part.js
@@ -1,19 +1,14 @@
 $.get('/party')
-.done(function( data ) {
-
-    //$('main').html( data );
-    $('.loading').html( '<i class="fa-solid fa-face-frown"></i><div class="japanese">取得できませんでした。</div><div>再読込してください。</div>' );
-
-}).fail( function() {
-  $('.loading').html( '<i class="fa-solid fa-face-frown"></i><div class="japanese">取得できませんでした。</div><div>再読込してください。</div>' );
-} )
+  .done(function(data) {
+    $('main').html(data);
+  }).fail(function() {
+    $('.loading').html('<i class="fa-solid fa-face-frown"></i><div class="japanese">取得できませんでした</div><div class="japanese">再読込してください</div>');
+  })
 
 $.get('/news')
-.done(function( data ) {
-  console.log(data);
-    $('.news-banner__content').html( data );
+  .done(function(data) {
+    $('.news-banner__content').html(data);
 
-}).fail( function() {
-    console.log(data);
-  $('.news-banner__content').html( '<p><i class="fa-solid fa-face-frown"></i>取得できませんでした。</p>');
-} )
+  }).fail(function() {
+    $('.news-banner__content').html('<p><i class="fa-solid fa-face-frown"></i>取得できませんでした。</p>');
+  })
