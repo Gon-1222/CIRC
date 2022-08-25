be85b0920b33fb28202a9033e933bb25be6ba92c
Gon-1222 <caramelpopsoft@gmail.com>
2022-08-25 18:23:19 +0900

    test

--- lazy.py
+++ lazy.py
@@ -3,7 +3,7 @@ import os
 import random
 import string
 from File import Gfile
-
+import time
 class Lazy(Gfile):
     data=[]
     def __init__(self):
@@ -20,9 +20,11 @@ class Lazy(Gfile):
         self.data = [''.join(random.choices(string.ascii_letters + string.digits, k=20))]
         print(self.save())
         print(self.data)
+        print(time.perf_counter())
         return self.data[0]
     def get_data(self):
         print("data:",self.data[0])
+        print(time.perf_counter())
         ret = self.data[0]
         self.data=["notallowed"]
         self.save()
