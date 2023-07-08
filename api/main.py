a95e7b64564bf0f967c65dd0f6de8841204cfee3
Gon-1222 <caramelpopsoft@gmail.com>
2023-07-08 15:41:52 +0900

    typo

--- api/main.py
+++ api/main.py
@@ -27,7 +27,7 @@ from manager import Manager
 from permission import permit
 from news import News
 from lazy import Lazy
-
+from All_data import All_data
 
 # 環境変数取得
 CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
