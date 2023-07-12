1e874862f6d63f627e1c476e1c512f30d6e7df33
Gon-1222 <99107342+Gon-1222@users.noreply.github.com>
2023-07-12 15:12:39 +0900

    Update lazy_part.js

--- api/static/js/lazy_part.js
+++ api/static/js/lazy_part.js
@@ -8,13 +8,25 @@ $.ajax({type:"GET",
     $('.loading').html('<i class="fa-solid fa-face-frown"></i><div class="japanese">取得できませんでした</div><div class="japanese">再読込してください</div>');
     //setTimeout(function(){$.ajax(this)}, 500 );
   })
+
+$.ajax({type:"GET",
+        url:'/news',
+        cache: false,
+        timeout: 5000})
+  .done(function(data) {
+    $(".news-banner__content2").addClass("news-banner__content");
+    $('.news-banner__content2').html(data).trigger('create');
+  }).fail(function() {
+    $(".news-banner__content2").addClass("news-banner__content");
+    $('.news-banner__content2').html('<p><i class="fa-solid fa-face-frown"></i>取得できませんでした。</p>');
+  })
 /*$.get('/party')
   .done(function(data) {
     $('main').html(data).trigger('create');
   }).fail(function() {
     $('.loading').html('<i class="fa-solid fa-face-frown"></i><div class="japanese">取得できませんでした</div><div class="japanese">再読込してください</div>');
   })*/
-
+/*
 $.get('/news')
   .done(function(data) {
     $(".news-banner__content2").addClass("news-banner__content");
@@ -23,3 +35,4 @@ $.get('/news')
     $(".news-banner__content2").addClass("news-banner__content");
     $('.news-banner__content2').html('<p><i class="fa-solid fa-face-frown"></i>取得できませんでした。</p>');
   })
+  */
